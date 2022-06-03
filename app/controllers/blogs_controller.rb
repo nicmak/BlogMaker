eacea39f14a5577b9c100d417f313047b51d63cf
nicmak <nicholas.mak@jane.app>
2022-06-03 15:07:40 -0700

    finished toggle status functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -58,6 +58,16 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    blog = Blog.friendly.find(params[:id])
+    if blog.published?
+       blog.draft!
+    elsif blog.draft?
+      blog.published!
+    end
+    redirect_to blogs_url, notice: 'Post status has been updated'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
