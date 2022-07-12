ecab031380086475415205a7d70a3f0679c6b050
nicmak <nicholas.mak@jane.app>
2022-07-11 23:37:29 -0700

    refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,9 +19,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :body, :subtitle, technologies_attributes: [
-      :name
-    ]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -39,7 +37,7 @@ class PortfoliosController < ApplicationController
   def update 
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :body, :subtitle))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "Portfolio edit was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,6 +56,19 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_path , notice: 'Portfolio destroyed'}
     end
+  end
+
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :title, 
+      :body, 
+      :subtitle, 
+      technologies_attributes: [
+        :name
+      ]
+    )
   end
 
 end
