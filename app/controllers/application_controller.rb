d423937fd1d924f96d34cea39493684c16ace41d
nicmak <nicholas.mak@jane.app>
2022-07-13 08:47:19 -0700

    revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super || guest_user
+  end
 end
