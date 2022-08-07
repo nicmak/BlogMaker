a81892e74c8640fc232db2ead728c89e9fad0945
nicmak <nicholas.mak@jane.app>
2022-08-07 11:44:17 -0700

    spacing

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,9 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
+  
   validates_presence_of :title, :body
+
   belongs_to :topic, optional: true
 
   def self.special_blogs
